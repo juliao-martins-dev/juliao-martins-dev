6cb28b6475edea3e06bff59092ee2536788b3494
Juliao Martins <juliao.martins@telmor.tl>
2026-01-24 09:12:48 +0900

    feat: don't need orbit control

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -1,22 +1,18 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
-import { OrbitControls } from '@react-three/drei'
+import { Suspense, useState } from 'react'
 import Model from "./Model"
 
 export default function Scene() {
-  return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <ambientLight intensity={0.8} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+    return (
+        <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+            <ambientLight intensity={0.8} />
+            <directionalLight position={[5, 5, 5]} intensity={1} />
 
-      <Suspense fallback={null}>
-        <Model />
-      </Suspense>
-
-      {/* Optional, bisa dihapus kalau tidak ingin kontrol manual */}
-      <OrbitControls enableZoom={false} />
-    </Canvas>
-  )
+            <Suspense fallback={null}>
+                <Model />
+            </Suspense>
+        </Canvas>
+    );
 }
