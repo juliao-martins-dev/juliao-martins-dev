afe16919707f0d3eb5e831c6cf95357f0f32ac5e
Julião Martins <indianmobiledeveloper022@gmail.com>
2026-02-02 16:49:50 +0900

    Update gallery.ts

---
+++ data/gallery.ts
@@ -0,0 +1,13 @@
+import { GalleryItem } from "@/types/gallery";
+
+
+export const galleryItems: GalleryItem[] = [
+  { src: "/1.jpg" },
+  { src: "/2.jpg" },
+  { src: "/3.jpg" },
+  { src: "/4.gif" },
+  { src: "/5.jpg" },
+  { src: "/6.gif" },
+  { src: "/7.jpg" },
+  { src: "/8.jpg" },
+];
