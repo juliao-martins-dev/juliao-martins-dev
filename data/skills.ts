c35946981671bb49fb2d50815f711c4ceb8b5013
Julião Martins <indianmobiledeveloper022@gmail.com>
2026-02-02 14:02:30 +0900

    Create skills.ts
    Remove skills data to data\skills.ts

---
+++ data/skills.ts
@@ -0,0 +1,48 @@
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiReact,
+  SiDjango,
+} from "react-icons/si";
+import { RiFirebaseFill } from "react-icons/ri";
+import { IoSparklesSharp } from "react-icons/io5";
+
+
+export const skills = [
+  {
+    name: "Tailwind CSS",
+    label: "Utility-first CSS",
+    icon: SiTailwindcss,
+    color: "#38BDF8",
+  },
+  {
+    name: "Next.js",
+    label: "React Framework",
+    icon: SiNextdotjs,
+    color: "#000000",
+  },
+  {
+    name: "GSAP",
+    label: "Animation Library",
+    icon: IoSparklesSharp,
+    color: "#88CE02",
+  },
+  {
+    name: "React Native",
+    label: "Mobile Development",
+    icon: SiReact,
+    color: "#61DAFB",
+  },
+  {
+    name: "Firebase",
+    label: "Backend as a Service",
+    icon: RiFirebaseFill,
+    color: "#FFCA28",
+  },
+  {
+    name: "Django",
+    label: "Backend Framework",
+    icon: SiDjango,
+    color: "#092E20",
+  },
+];
