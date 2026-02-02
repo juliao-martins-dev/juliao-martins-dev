af8e7e72e3f4752cb08035ac07290463dcca4a39
Julião Martins <indianmobiledeveloper022@gmail.com>
2026-02-02 16:50:57 +0900

    refactor(page): extract Home section into components/sections/Home.tsx

---
+++ components/sections/Home.tsx
@@ -0,0 +1,21 @@
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+export default function Home() {
+  const t = useTranslations();
+  
+  return (
+    <section
+        id="home"
+        className="min-h-screen flex items-center justify-center bg-gray-50"
+      >
+      <div className="text-center">
+        <Image src="/juliao_martins.jpg" priority alt="Juliao Martins" width={200} height={200} className="w-52 h-52 mx-auto mb-3 object-cover object-top rounded-full border-2 border-red-950" />
+        <h2 className="text-4xl font-bold mb-4">{t("home.title")}</h2>
+        <p className="text-gray-600 mb-6">
+          {t("home.subtitle")}
+        </p>
+      </div>
+    </section>
+  );
+}
