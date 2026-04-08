25fcbf55b0c87593d4e6bca00e41aa46aa354ec1
Julião Martins <indianmobiledeveloper022@gmail.com>
2026-04-08 20:34:43 +0900

    feat(api): create axios instance

--- lib/api/contact.ts
+++ lib/api/contact.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
 import { ContactFormData } from "@/types/contact";
+import { api } from "./client";
 
 
 export async function sendContactForm(data: ContactFormData) {
-  return axios.post(
-        "https://sheetdb.io/api/v1/iswzlnv05hdq5",
+  return api.post(
+        "/",
         {
           data: data
         },
